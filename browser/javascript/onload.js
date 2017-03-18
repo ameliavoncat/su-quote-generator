baee42f4c7ad58303a3702f46f607137e29cd682
Amelia Clarke <ameliavoncat@gmail.com>
2017-03-17 22:09:05 -0700

    Cleaned up page positioning and started responsive design

--- browser/javascript/onload.js
+++ browser/javascript/onload.js
@@ -2,7 +2,7 @@ window.onload = function(){
   var quoteGenerator = new QuoteGenerator()
   QuoteGenerator.loadJSONForInstance(quoteGenerator, './browser/data/quotes.json')
 
-  document.querySelector("button.quote-box__button").addEventListener('click', function(){
+  document.querySelector("button.quote-button").addEventListener('click', function(){
     quoteGenerator.generateQuote();
     quoteGenerator.displayQuote();
   });
